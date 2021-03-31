4156fa2c4641f8ddafb2a6db483665c0e970b0f4
wangpeialex <alexwangpeipei@gmail.com>
2021-03-31 22:33:34 +0800

    Experimenting on navigation bar

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -9,6 +9,31 @@
             <li><router-link class="router" to="/settings" exact>Settings</router-link> </li>
         </ul>
     </nav>
+
+    <Menu :theme="theme3" active-name="1" style="position: relative;top:300px;">
+        <MenuGroup title="内容管理">
+            <MenuItem name="1">
+                <Icon type="document-text"></Icon>
+                文章管理
+            </MenuItem>
+            <MenuItem name="2">
+                <Icon type="chatbubbles"></Icon>
+                评论管理
+            </MenuItem>
+        </MenuGroup>
+        <MenuGroup title="统计分析">
+            <MenuItem name="3">
+                <Icon type="heart"></Icon>
+                用户留存
+            </MenuItem>
+            <MenuItem name="4">
+                <Icon type="heart-broken"></Icon>
+                流失用户
+            </MenuItem>
+        </MenuGroup>
+    </Menu>
+
+
     <router-view class="page-view"></router-view>
 </div>
 </template>
